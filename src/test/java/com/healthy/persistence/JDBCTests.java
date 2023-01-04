bf46a96ca56969cc0909abe679adf1382eb39c33
study <zeroize01@gmail.com>
2023-01-05 00:12:14 +0900

    standard

---
+++ src/test/java/com/healthy/persistence/JDBCTests.java
@@ -0,0 +1,34 @@
+package com.healthy.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306",
+				"root", "1234")) {
+			log.info(conn);
+		} catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
